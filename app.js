a13886ff836ab7d100fc175e0ddc8e84de506533
Moumenh <mohanbali94@gmail.com>
2020-11-02 16:36:58 +0200

    some functionality

--- app.js
+++ app.js
@@ -4,17 +4,52 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const cells = document.querySelectorAll('.game-cell')
 
-
     let xIsNext = true
 
+    const WINNING_COMBINATIONS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    const checkWin = (currentClass) => {
+        return WINNING_COMBINATIONS.some(combination => {
+            return combination.every(index => {
+                return cells[index].classList.contains(currentClass)
+            })
+        })
+    }
+
+
+
     const handleCellClick = (e) => {
+
+        const classList = e.target.classList
+
+        if (classList[1] === 'x' || classList[1] === 'o') {
+            return
+        }
+
         if (xIsNext) {
             e.target.classList.add('x')
+            if(checkWin('x')){
+                return alert('player x is won')
+            }
             xIsNext = !xIsNext
+            status.innerHTML = `o is next`
 
         } else {
             e.target.classList.add('o')
+            if(checkWin('o')){
+                alert('player o is won')
+            }
             xIsNext = !xIsNext
+            status.innerHTML = `x is next`
         }
     }
 
@@ -25,13 +60,13 @@ document.addEventListener('DOMContentLoaded', () => {
             cell.classList.remove('x')
             cell.classList.remove('o')
         })
-      }
+    }
 
 
 
-    cells.forEach( (cell) => {
-        cell.addEventListener('click' , handleCellClick)
-    } )
+    cells.forEach((cell) => {
+        cell.addEventListener('click', handleCellClick)
+    })
 
     reset.addEventListener('click', handleReset)
 
